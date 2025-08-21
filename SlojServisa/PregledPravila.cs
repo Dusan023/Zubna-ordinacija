3b7799e0f280cb73bcbe19c8547bbb700ea010d4
mihailo.tanackov <mihailo.tanackov@gmail.com>
2025-08-21 21:41:59 +0200

    Pravljenje mosta između wpf behind koda i PregledDB, treba ugraditi pravila

--- SlojServisa/PregledPravila.cs
+++ SlojServisa/PregledPravila.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using SlojPodataka.Klase;
+using SlojPodataka;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Klase;
 
 namespace SlojServisa
 {
     public class PregledPravila
     {
+        
+            private readonly PregledDB _repo;
+
+            public PregledPravila()
+            {
+                _repo = new PregledDB();
+            }
+
+            public List<Pregled> VratiSvePreglede() => _repo.GetAll();
+            public void DodajPregled(Pregled pregled) => _repo.Insert(pregled);
+            public void IzmeniPregled(Pregled pregled) => _repo.Update(pregled);
+            public void ObrisiPregled(int id) => _repo.Delete(id);
+        
     }
 }
