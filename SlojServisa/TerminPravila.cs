6b0d0dcbd2c699589d7894def75159c9bfb8d2cd
mihailo.tanackov <mihailo.tanackov@gmail.com>
2025-08-23 00:22:28 +0200

    Trenutno samo most između UI i DB

--- SlojServisa/TerminPravila.cs
+++ SlojServisa/TerminPravila.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Klase;
+using SlojPodataka;
+using SlojPodataka.Klase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,18 @@ using System.Threading.Tasks;
 
 namespace SlojServisa
 {
-    internal class TerminPravila
+    public class TerminPravila
     {
+        private readonly TerminDB _repo;
+
+        public TerminPravila()
+        {
+            _repo = new TerminDB();
+        }
+
+        public List<Termin> VratiSveTermine() => _repo.GetAll();
+        public void DodajTermin(Termin termin) => _repo.Insert(termin);
+        public void IzmeniTermin(Termin termin) => _repo.Update(termin);
+        public void ObrisiTermin(int id) => _repo.Delete(id);
     }
 }
