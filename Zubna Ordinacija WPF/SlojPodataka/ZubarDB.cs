219d8fea1654e8375d8bcac0e5f15366bf98ac3a
Kristijan Kečkeš <kristijankeckes93@gmail.com>
2025-08-22 14:40:54 +0200

    pacijenti i zubar

---
+++ Zubna Ordinacija WPF/SlojPodataka/ZubarDB.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlojPodataka.Klase;
+
+namespace SlojPodataka
+{
+    public class ZubarDB
+    {
+        private readonly string _connectionString;
+
+        public ZubarDB()
+        {
+            _connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Zubna_Ordinacija;Integrated Security=True";
+        }
+        public List<Zubar> GetAll()
+        {
+            var lista = new List<Zubar>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Zubar", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    lista.Add(new Zubar
+                    {
+                        IDZubara = (int)reader["IDZubara"],
+                        Ime = reader["Ime"].ToString(),
+                        Prezime = reader["Prezime"].ToString(),
+                        JMBG = reader["JMBG"].ToString(),
+                        BrojTelefona = reader["BrojTelefona"].ToString(),
+                        Email = reader["Email"].ToString()
+                    });
+                }
+            }
+            return lista;
+        }
+
+        public void Insert(Zubar zubar)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO Zubar (Ime, Prezime, JMBG, BrojTelefona, Email) " +
+                    "VALUES (@Ime, @Prezime, @JMBG, @BrojTelefona, @Email)", conn);
+
+                cmd.Parameters.AddWithValue("@Ime", zubar.Ime);
+                cmd.Parameters.AddWithValue("@Prezime", zubar.Prezime);
+                cmd.Parameters.AddWithValue("@JMBG", zubar.JMBG);
+                cmd.Parameters.AddWithValue("@BrojTelefona", zubar.BrojTelefona);
+                cmd.Parameters.AddWithValue("@Email", zubar.Email);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(Zubar zubar)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "UPDATE Zubar SET Ime=@Ime, Prezime=@Prezime, JMBG=@JMBG, " +
+                    "BrojTelefona=@BrojTelefona, Email=@Email WHERE IDZubara=@IDZubara", conn);
+
+                cmd.Parameters.AddWithValue("@IDZubara", zubar.IDZubara);
+                cmd.Parameters.AddWithValue("@Ime", zubar.Ime);
+                cmd.Parameters.AddWithValue("@Prezime", zubar.Prezime);
+                cmd.Parameters.AddWithValue("@JMBG", zubar.JMBG);
+                cmd.Parameters.AddWithValue("@BrojTelefona", zubar.BrojTelefona);
+                cmd.Parameters.AddWithValue("@Email", zubar.Email);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Zubar WHERE IDZubara=@IDZubara", conn);
+                cmd.Parameters.AddWithValue("@IDZubara", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
