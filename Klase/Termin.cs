1f8ef2aef2df5a42e0dc9f2058b2120093f9ddaf
mihailo.tanackov <mihailo.tanackov@gmail.com>
2025-08-23 00:19:01 +0200

    Termin klasa

--- Klase/Termin.cs
+++ Klase/Termin.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Klase
 {
-    internal class Termin
+    public class Termin
     {
+        public int IDTermina { get; set; }       // ID termina
+        public DateTime Datum { get; set; }      // Datum termina
+        public string VrstaUsluge { get; set; }  // Vrsta usluge
+        public int IDPacijenta { get; set; }     // ID pacijenta
+        public int IDZubara { get; set; }        // ID zubara
+        public TimeSpan Vreme { get; set; }      // Vreme termina
     }
 }
