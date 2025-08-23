84e96183d5536e788d10ed8dbee1a0cb9fee98f2
mihailo.tanackov <mihailo.tanackov@gmail.com>
2025-08-23 03:38:41 +0200

    Merge update (prepravljanje solucije nastalom kao greška ne ažuriranih .csproj fajlova po slojevima)

---
+++ SlojServisa/PacijentiPravila.cs
@@ -0,0 +1,26 @@
+﻿using SlojPodataka;
+using SlojPodataka.Klase;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlojServisa //
+{
+    public class PacijentPravila
+    {
+        private readonly PacijentiDB _repo;
+
+        public PacijentPravila()
+        {
+            _repo = new PacijentiDB();
+        }
+
+        public List<Pacijenti> VratiSvePacijente() => _repo.GetAll();
+        public void DodajPacijenta(Pacijenti pacijent) => _repo.Insert(pacijent);
+        public void IzmeniPacijenta(Pacijenti pacijent) => _repo.Update(pacijent);
+        public void ObrisiPacijenta(int id) => _repo.Delete(id);
+    }
+}
